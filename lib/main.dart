317d1b818c04e89fab8afbe83d1a442dabf528bc
Justin K -W10 <justinjoseph0007@gmail.com>
2019-03-23 16:10:36 -0400

    Got an immutabble page 1 ready

--- lib/main.dart
+++ lib/main.dart
@@ -17,25 +17,97 @@ class FirstScreen extends StatelessWidget {
   Widget build(BuildContext ctxt) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Multi Page Application")),
-      body: SizedBox(
-        width: double.infinity,
-        height: 150.0,
-        child: InkWell(
-          onTap: () {},
-          child: Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const ListTile(
-                  leading: Icon(Icons.looks_one),
-                  title: Text('The Enchanted Nightingale'),
-                  subtitle:
-                      Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+      body: new Column(
+        children: <Widget>[
+          new Container(
+            margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 40),
+            child: new SizedBox(
+              width: double.infinity,
+              height: 130.0,
+              child: InkWell(
+                onTap: () {},
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.looks_one),
+                        title: Text('Week 1'),
+                        subtitle: Text('3 sets of 5 reps'),
+                      ),
+                    ], // End of list
+                  ),
                 ),
-              ], // End of list
+              ),
             ),
           ),
-        ),
+          new Container(
+            margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 10),
+            child: new SizedBox(
+              width: double.infinity,
+              height: 130.0,
+              child: InkWell(
+                onTap: () {},
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.looks_two),
+                        title: Text('Week 2'),
+                        subtitle: Text('3 sets of 3 reps'),
+                      ),
+                    ], // End of list
+                  ),
+                ),
+              ),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 10),
+            child: new SizedBox(
+              width: double.infinity,
+              height: 130.0,
+              child: InkWell(
+                onTap: () {},
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.looks_3),
+                        title: Text('Week 3'),
+                        subtitle: Text('5/3/1 sets'),
+                      ),
+                    ], // End of list
+                  ),
+                ),
+              ),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 10),
+            child: new SizedBox(
+              width: double.infinity,
+              height: 130.0,
+              child: InkWell(
+                onTap: () {},
+                child: Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.looks_4),
+                        title: Text('Week 4'),
+                        subtitle: Text('Deload week'),
+                      ),
+                    ], // End of list
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ], // List of cards end here
       ),
     );
   }
