7701d59a2bd93da318adc092655f52477d59c1b5
Justin K -W10 <justinjoseph0007@gmail.com>
2019-03-23 19:11:13 -0400

    Learned how to re-use widget using a factory

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,43 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Weeks extends StatelessWidget {
+  Weeks({this.argTitle, this.subTitle, this.cardIcon});
+  final Widget argTitle;
+  final Widget subTitle;
+  final Icon cardIcon;
+  @override
+  Widget build(BuildContext ctxt) {
+    return (new Container(
+      margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 10),
+      child: new SizedBox(
+        width: double.infinity,
+        height: 144.0,
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              ctxt,
+              new MaterialPageRoute(builder: (ctxt) => new SecondScreen()),
+            );
+          },
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: cardIcon,
+                  title: argTitle,
+                  subtitle: subTitle,
+                ),
+              ], // End of list
+            ),
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
 class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext ctxt) {
@@ -19,133 +56,25 @@ class FirstScreen extends StatelessWidget {
       appBar: new AppBar(title: new Text("Multi Page Application")),
       body: new Column(
         children: <Widget>[
-          new Container(
-            margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 10),
-            child: new SizedBox(
-              width: double.infinity,
-              height: 144.0,
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    ctxt,
-                    new MaterialPageRoute(
-                        builder: (ctxt) => new SecondScreen()),
-                  );
-                },
-                child: Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const ListTile(
-                        leading: Icon(
-                          Icons.looks_one,
-                          color: Colors.blue,
-                          size: 50.0,
-                        ),
-                        title: Text('Week 1'),
-                        subtitle: Text('3 sets of 5 reps'),
-                      ),
-                    ], // End of list
-                  ),
-                ),
-              ),
-            ),
+          new Weeks(
+            argTitle: new Text("Week 1"),
+            subTitle: new Text("3 sets of 5 reps"),
+            cardIcon: new Icon(Icons.looks_one, color: Colors.blue, size: 50.0),
           ),
-          new Container(
-            margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 10),
-            child: new SizedBox(
-              width: double.infinity,
-              height: 144.0,
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    ctxt,
-                    new MaterialPageRoute(
-                        builder: (ctxt) => new SecondScreen()),
-                  );
-                },
-                child: Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const ListTile(
-                        leading: Icon(
-                          Icons.looks_two,
-                          color: Colors.blue,
-                          size: 50.0,
-                        ),
-                        title: Text('Week 2'),
-                        subtitle: Text('3 sets of 3 reps'),
-                      ),
-                    ], // End of list
-                  ),
-                ),
-              ),
-            ),
+          new Weeks(
+            argTitle: new Text("Week 2"),
+            subTitle: new Text("3 sets of 3 reps"),
+            cardIcon: new Icon(Icons.looks_two, color: Colors.blue, size: 50.0),
           ),
-          new Container(
-            margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 10),
-            child: new SizedBox(
-              width: double.infinity,
-              height: 144.0,
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    ctxt,
-                    new MaterialPageRoute(
-                        builder: (ctxt) => new SecondScreen()),
-                  );
-                },
-                child: Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const ListTile(
-                        leading: Icon(
-                          Icons.looks_3,
-                          color: Colors.blue,
-                          size: 50.0,
-                        ),
-                        title: Text('Week 3'),
-                        subtitle: Text('5/3/1 sets'),
-                      ),
-                    ], // End of list
-                  ),
-                ),
-              ),
-            ),
+          new Weeks(
+            argTitle: new Text("Week 3"),
+            subTitle: new Text("5/3/1 sets"),
+            cardIcon: new Icon(Icons.looks_3, color: Colors.blue, size: 50.0),
           ),
-          new Container(
-            margin: new EdgeInsets.only(left: 10.0, right: 10.0, top: 10),
-            child: new SizedBox(
-              width: double.infinity,
-              height: 144.0,
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                    ctxt,
-                    new MaterialPageRoute(
-                        builder: (ctxt) => new SecondScreen()),
-                  );
-                },
-                child: Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const ListTile(
-                        leading: Icon(
-                          Icons.looks_4,
-                          color: Colors.blue,
-                          size: 50.0,
-                        ),
-                        title: Text('Week 4'),
-                        subtitle: Text('Deload week'),
-                      ),
-                    ], // End of list
-                  ),
-                ),
-              ),
-            ),
+          new Weeks(
+            argTitle: new Text("Week 4"),
+            subTitle: new Text("Deload week"),
+            cardIcon: new Icon(Icons.looks_4, color: Colors.blue, size: 50.0),
           ),
         ], // List of cards end here
       ),
