aa88fc861d049e8fc84b0ab536a8d8bd59f5b9df
Justin K -W10 <justinjoseph0007@gmail.com>
2019-03-30 08:50:32 -0400

    Made activity cards into a widget of its own

--- lib/activity_page.dart
+++ lib/activity_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'save_state.dart';
+import 'activity_widget.dart';
 
 // This is the page where the four activities of the week are displayed
 class ActivityPage extends StatelessWidget {
@@ -33,124 +34,32 @@ class ActivityPage extends StatelessWidget {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Expanded(
-                  child: SizedBox(
-                    height: 250,
-                    child: Material(
-                      color: Colors.purple[200],
-                      elevation: 4.0,
-                      child: Stack(
-                        children: [
-                          Center(
-                            child: Icon(Icons.beenhere,
-                                color: Colors.white.withOpacity(0.5),
-                                size: 150.0),
-                          ),
-                          Positioned.fill(
-                            child: Ink.image(
-                              image: AssetImage('assets/squat.png'),
-                              width: 120.0,
-                              child: InkWell(
-                                onTap: saveData,
-                                splashColor: Colors.red[200],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                new Activity(
+                  action: saveData,
+                  image: 'assets/squat.png',
+                  color: Colors.purple[200],
                 ),
                 SizedBox(width: 10.0),
-                Expanded(
-                  child: SizedBox(
-                    height: 250,
-                    child: Material(
-                      color: Colors.green[200],
-                      elevation: 4.0,
-                      child: Stack(
-                        children: [
-                          Center(
-                            child: Icon(Icons.beenhere,
-                                color: Colors.white.withOpacity(0.5),
-                                size: 150.0),
-                          ),
-                          Positioned.fill(
-                            child: Ink.image(
-                              image: AssetImage('assets/bench.png'),
-                              width: 120.0,
-                              child: InkWell(
-                                onTap: saveData,
-                                splashColor: Colors.red[200],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                new Activity(
+                  action: saveData,
+                  image: 'assets/bench.png',
+                  color: Colors.green[200],
                 ),
               ],
             ),
             SizedBox(height: 10.0),
             Row(
               children: <Widget>[
-                Expanded(
-                  child: SizedBox(
-                    height: 250,
-                    child: Material(
-                      color: Colors.blue[200],
-                      elevation: 4.0,
-                      child: Stack(
-                        children: [
-                          Center(
-                            child: Icon(Icons.beenhere,
-                                color: Colors.white.withOpacity(0.5),
-                                size: 150.0),
-                          ),
-                          Positioned.fill(
-                            child: Ink.image(
-                              image: AssetImage('assets/deadlift.png'),
-                              width: 120.0,
-                              child: InkWell(
-                                onTap: saveData,
-                                splashColor: Colors.red[200],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                new Activity(
+                  action: saveData,
+                  image: 'assets/deadlift.png',
+                  color: Colors.blue[200],
                 ),
                 SizedBox(width: 10.0),
-                Expanded(
-                  child: SizedBox(
-                    height: 250,
-                    child: Material(
-                      color: Colors.yellow[200],
-                      elevation: 4.0,
-                      child: Stack(
-                        children: [
-                          Center(
-                            child: Icon(Icons.beenhere,
-                                color: Colors.white.withOpacity(0.5),
-                                size: 150.0),
-                          ),
-                          Positioned.fill(
-                            child: Ink.image(
-                              image: AssetImage('assets/press.png'),
-                              width: 120.0,
-                              child: InkWell(
-                                onTap: saveData,
-                                splashColor: Colors.red[200],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                new Activity(
+                  action: saveData,
+                  image: 'assets/press.png',
+                  color: Colors.yellow[200],
                 ),
               ],
             ),
@@ -160,18 +69,3 @@ class ActivityPage extends StatelessWidget {
     );
   }
 }
-
-// // class for each exercise to be done
-// class Activity extends StatefulWidget {
-//   _ActivityState createState() => _ActivityState();
-// }
-
-// class _ActivityState extends State<Activity> {
-//   void notifyReceiver() {
-//     setState(() {
-//       // Empty method just to refresh widget, collecting new data is handled by
-//       // FutureBuilder. It gets data from memory and loads a default if it
-//       // doens't find any
-//     });
-//   }
-// }
---
+++ lib/activity_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+//////////////////////////////////////////////////////////////////////////////////////
+// This is the widget that displays a picture of an exercise and is clickable which //
+// take you to the next page.                                                       //
+// This widget also have a done state.                                              //
+//////////////////////////////////////////////////////////////////////////////////////
+
+class Activity extends StatefulWidget {
+  Activity({
+    Key key,
+    this.image,
+    this.action,
+    this.color,
+  }) : super(key: key);
+
+  final Color color;
+  final String image;
+  final Function action;
+  @override
+  _ActivityWidgetState createState() => _ActivityWidgetState();
+}
+
+class _ActivityWidgetState extends State<Activity> {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Expanded(
+      child: SizedBox(
+        height: 250,
+        child: Material(
+          color: widget.color,
+          elevation: 4.0,
+          child: Stack(
+            children: [
+              Center(
+                child: Icon(Icons.beenhere,
+                    color: Colors.white.withOpacity(0.5), size: 150.0),
+              ),
+              Positioned.fill(
+                child: Ink.image(
+                  image: AssetImage(widget.image),
+                  width: 120.0,
+                  child: InkWell(
+                    onTap: widget.action,
+                    splashColor: Colors.red[200],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
