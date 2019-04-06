848ce6241a82ae685126638a8cb2fd603c676ca7
Justin K -W10 <justinjoseph0007@gmail.com>
2019-04-06 18:35:45 -0400

    I will firebase in somethign else

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctxt) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false, // This will automatically go away during release build
       theme: ThemeData(
         primarySwatch: MaterialColor(Colors.red.shade200.value, {
           50: Colors.red.shade50,
