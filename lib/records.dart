199bf48593c2e3cb0891be99403a446f05a74c6f
Justin K -W10 <justinjoseph0007@gmail.com>
2019-04-13 18:59:54 -0400

    Added auto increment button to records page

--- lib/records.dart
+++ lib/records.dart
@@ -1,7 +1,43 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'save_state.dart';
 
-class RecordsPage extends StatelessWidget {
+class _RecordsPageState extends State<RecordsPage> {
+  int _squatRM = 0;
+  int _benchRM = 0;
+  int _deadliftRM = 0;
+  int _pressRM = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData(); // Load all the Max rep values from memory async
+  }
+
+  void _loadData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      // If no data exist return 0
+      _squatRM = (prefs.getInt("squat") ?? 0);
+      _benchRM = (prefs.getInt("bench") ?? 0);
+      _deadliftRM = (prefs.getInt("deadlift") ?? 0);
+      _pressRM = (prefs.getInt("press") ?? 0);
+    });
+  }
+
+  IncrementandRefreshParent() {
+    _squatRM += 10;
+    _benchRM += 5;
+    _deadliftRM += 10;
+    _pressRM += 5;
+    SaveStateHelper.setMaxRepToMemory('squat', _squatRM);
+    SaveStateHelper.setMaxRepToMemory('bench', _benchRM);
+    SaveStateHelper.setMaxRepToMemory('deadlift', _deadliftRM);
+    SaveStateHelper.setMaxRepToMemory('press', _pressRM);
+    // Refersh widget
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext ctxt) {
     return new Scaffold(
@@ -12,97 +48,65 @@ class RecordsPage extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              FutureBuilder<int>(
-                // This is where Future is trying to get data from
-                future: SaveStateHelper.getMaxRepFromMemory('squat'),
-                initialData: 0,
-                builder: (BuildContext ctxt, AsyncSnapshot<int> snapshot) {
-                  if (snapshot.hasError) {
-                    return TextField(); // Show empty textfield
-                  } else {
-                    return TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          hintText:
-                              'Enter 1RM for squat (Current: ${snapshot.data} lbs)'),
-                      onSubmitted: (value) {
-                        SaveStateHelper.setMaxRepToMemory(
-                            'squat', int.parse(value));
-                      },
-                    );
-                  }
-                }, // End of  builder
-              ), // End of FutureBuilder
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: 'Enter 1RM for squat (Current: ${_squatRM} lbs)'),
+                onSubmitted: (value) {
+                  SaveStateHelper.setMaxRepToMemory('squat', int.parse(value));
+                },
+              ),
               SizedBox(height: 30),
-              FutureBuilder<int>(
-                // This is where Future is trying to get data from
-                future: SaveStateHelper.getMaxRepFromMemory('bench'),
-                initialData: 0,
-                builder: (BuildContext ctxt, AsyncSnapshot<int> snapshot) {
-                  if (snapshot.hasError) {
-                    return TextField(); // Show empty textfield
-                  } else {
-                    return TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          hintText:
-                              'Enter 1RM for bench (Current: ${snapshot.data} lbs)'),
-                      onSubmitted: (value) {
-                        SaveStateHelper.setMaxRepToMemory(
-                            'bench', int.parse(value));
-                      },
-                    );
-                  }
-                }, // End of  builder
-              ), // End of FutureBuilder
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: 'Enter 1RM for bench (Current: ${_benchRM} lbs)'),
+                onSubmitted: (value) {
+                  SaveStateHelper.setMaxRepToMemory('bench', int.parse(value));
+                },
+              ),
               SizedBox(height: 30),
-              FutureBuilder<int>(
-                // This is where Future is trying to get data from
-                future: SaveStateHelper.getMaxRepFromMemory('deadlift'),
-                initialData: 0,
-                builder: (BuildContext ctxt, AsyncSnapshot<int> snapshot) {
-                  if (snapshot.hasError) {
-                    return TextField(); // Show empty textfield
-                  } else {
-                    return TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          hintText:
-                              'Enter 1RM for deadlift (Current: ${snapshot.data} lbs)'),
-                      onSubmitted: (value) {
-                        SaveStateHelper.setMaxRepToMemory(
-                            'deadlift', int.parse(value));
-                      },
-                    );
-                  }
-                }, // End of  builder
-              ), // End of FutureBuilder
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText:
+                        'Enter 1RM for deadlift (Current: ${_deadliftRM} lbs)'),
+                onSubmitted: (value) {
+                  SaveStateHelper.setMaxRepToMemory(
+                      'deadlift', int.parse(value));
+                },
+              ),
               SizedBox(height: 30),
-              FutureBuilder<int>(
-                // This is where Future is trying to get data from
-                future: SaveStateHelper.getMaxRepFromMemory('press'),
-                initialData: 0,
-                builder: (BuildContext ctxt, AsyncSnapshot<int> snapshot) {
-                  if (snapshot.hasError) {
-                    return TextField(); // Show empty textfield
-                  } else {
-                    return TextField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          hintText:
-                              'Enter 1RM for press (Current: ${snapshot.data} lbs)'),
-                      onSubmitted: (value) {
-                        SaveStateHelper.setMaxRepToMemory(
-                            'press', int.parse(value));
-                      },
-                    );
-                  }
-                }, // End of  builder
-              ), // End of FutureBuilder
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: 'Enter 1RM for press (Current: ${_pressRM} lbs)'),
+                onSubmitted: (value) {
+                  SaveStateHelper.setMaxRepToMemory('press', int.parse(value));
+                },
+              ),
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          IncrementandRefreshParent();
+        },
+        icon: Icon(Icons.add),
+        label: Text("Increment"),
+        tooltip: "Auto increment all values",
+        backgroundColor: Colors.red[400],
+      ),
     );
   }
+}
+
+class RecordsPage extends StatefulWidget {
+  RecordsPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _RecordsPageState createState() => _RecordsPageState();
 }
