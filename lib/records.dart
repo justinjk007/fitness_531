b0d930668bc184d9ed8dd799ce83fed6aeec2be1
Justin K -W10 <justinjoseph0007@gmail.com>
2019-04-14 19:11:50 -0400

    Oh yes, best customizations for the text fields

--- lib/records.dart
+++ lib/records.dart
@@ -38,6 +38,22 @@ class _RecordsPageState extends State<RecordsPage> {
     setState(() {});
   }
 
+  // One controller for weach text field
+  final _controller1 = TextEditingController();
+  final _controller2 = TextEditingController();
+  final _controller3 = TextEditingController();
+  final _controller4 = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    _controller1.dispose();
+    _controller2.dispose();
+    _controller3.dispose();
+    _controller4.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext ctxt) {
     return new Scaffold(
@@ -46,43 +62,66 @@ class _RecordsPageState extends State<RecordsPage> {
       body: Padding(
         padding: EdgeInsets.only(top: 50, right: 20, left: 20),
         child: Center(
-          child: Column(
+          child: ListView(
             children: [
               TextField(
+                controller: _controller1,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                    hintText: 'Enter 1RM for squat (Current: ${_squatRM} lbs)'),
+                    labelText:
+                        'Enter 1RM for squat (Current: ${_squatRM} lbs)'),
                 onSubmitted: (value) {
                   SaveStateHelper.setMaxRepToMemory('squat', int.parse(value));
+                  _squatRM = int.parse(value);
+                  // This will clear the text input so new value is displayed
+                  _controller1.clear();
+                  setState(() {}); // Refersh
                 },
               ),
               SizedBox(height: 30),
               TextField(
+                controller: _controller2,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                    hintText: 'Enter 1RM for bench (Current: ${_benchRM} lbs)'),
+                    labelText:
+                        'Enter 1RM for bench (Current: ${_benchRM} lbs)'),
                 onSubmitted: (value) {
                   SaveStateHelper.setMaxRepToMemory('bench', int.parse(value));
+                  _benchRM = int.parse(value);
+                  // This will clear the text input so new value is displayed
+                  _controller2.clear();
+                  setState(() {}); // Refersh
                 },
               ),
               SizedBox(height: 30),
               TextField(
+                controller: _controller3,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                    hintText:
+                    labelText:
                         'Enter 1RM for deadlift (Current: ${_deadliftRM} lbs)'),
                 onSubmitted: (value) {
                   SaveStateHelper.setMaxRepToMemory(
                       'deadlift', int.parse(value));
+                  _deadliftRM = int.parse(value);
+                  // This will clear the text input so new value is displayed
+                  _controller3.clear();
+                  setState(() {}); // Refersh
                 },
               ),
               SizedBox(height: 30),
               TextField(
+                controller: _controller4,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                    hintText: 'Enter 1RM for press (Current: ${_pressRM} lbs)'),
+                    labelText:
+                        'Enter 1RM for press (Current: ${_pressRM} lbs)'),
                 onSubmitted: (value) {
                   SaveStateHelper.setMaxRepToMemory('press', int.parse(value));
+                  _pressRM = int.parse(value);
+                  // This will clear the text input so new value is displayed
+                  _controller4.clear();
+                  setState(() {}); // Refersh
                 },
               ),
             ],
