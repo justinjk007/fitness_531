a5e02f5ecc5d019df00049bdab67600d8e69a45b
Justin K -Mac <justinjoseph0007@gmail.com>
2019-05-15 18:04:59 -0400

    Planning on adding progress indicators for page1, commiting for later

--- lib/week_widget.dart
+++ lib/week_widget.dart
@@ -1,8 +1,31 @@
+import 'package:percent_indicator/percent_indicator.dart';
 import 'package:flutter/material.dart';
 import 'save_state.dart';
 import 'activity_page.dart';
 
 class _WeekWidgetState extends State<Weeks> {
+
+  int _week1Status = 0;
+  int _week2Status = 0;
+  int _week3Status = 0;
+  int _week4Status = 0;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _loadMaxRep(); // Load all the Max rep values from memory async
+  // }
+
+  // void _loadMaxRep() async {
+  //   final SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   setState(() {
+  //     // If no data exist return 0
+  //     _maxRep = (prefs.getInt(widget.activity) ?? 0);
+  //     _assistanceMaxRep =
+  //         (prefs.getInt(getAssistanceActivity(widget.activity)) ?? 0);
+  //   });
+  // }
+
   void notifyReceiver() {
     setState(() {
       // Empty method just to refresh widget, collecting new data is handled by
@@ -15,9 +38,22 @@ class _WeekWidgetState extends State<Weeks> {
 
   Widget _getNextScreen() {
     Widget nextScreen = new ActivityPage(
-      // notifyParent: notifyReceiver,
-      weekIDForChild: widget.weekID);
+        // notifyParent: notifyReceiver,
+        weekIDForChild: widget.weekID);
     return nextScreen;
+  }
+
+  Widget _getProgressWidget(double percent) {
+    Widget progress = new CircularPercentIndicator(
+      radius: 45.0,
+      lineWidth: 7.0,
+      animation: true,
+      animationDuration: 1000,
+      percent: percent/100,
+      circularStrokeCap: CircularStrokeCap.round,
+      progressColor: Colors.red[300],
+    );
+    return progress;
   }
 
   @override
@@ -47,17 +83,19 @@ class _WeekWidgetState extends State<Weeks> {
                     builder:
                         (BuildContext context, AsyncSnapshot<bool> snapshot) {
                       if (snapshot.hasError) {
-                        return Icon(Icons.check_box_outline_blank,
-                            color: Colors.red[300], size: 50.0);
+                        // return Icon(Icons.check_box_outline_blank,
+                        //     color: Colors.red[300], size: 50.0);
+                        return _getProgressWidget(0.0);
                       } else {
                         if (snapshot.data == true) {
                           // Item is marked undone
-                          return Icon(Icons.check_box_outline_blank,
-                              color: Colors.red[300], size: 50.0);
+                          // return Icon(Icons.check_box_outline_blank,
+                          //     color: Colors.red[300], size: 50.0);
+                          return _getProgressWidget(50);
                         } else {
                           // Item is marked done
-                          return Icon(Icons.check_box,
-                              color: Colors.red[300], size: 50.0);
+                          return Icon(Icons.check_circle,
+                              color: Colors.red[300], size: 52.0);
                         }
                       }
                     }, // End of  builder
