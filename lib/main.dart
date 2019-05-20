20785e5add40065a88989c8e0a52c85dbc115f44
Justin K -W10 <justinjoseph0007@gmail.com>
2019-05-20 11:21:13 -0400

    Added 2 more color elements

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,8 @@ class MyApp extends StatelessWidget {
       themedWidgetBuilder: (context, theme) {
         return new MaterialApp(
           theme: theme,
+          darkTheme: darkTheme, // Used in Android > 9, if user turns on system wide dark mode
+          color: Theme.of(context).primaryColor,
           home: new WeekPage(),
         );
       },
