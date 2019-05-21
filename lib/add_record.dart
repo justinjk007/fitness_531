d36b067d5caf692427d4fbe53df0e75766934494
Justin K -Mac <justinjoseph0007@gmail.com>
2019-05-21 18:39:05 -0400

    Can add data from Add records page now

--- lib/add_record.dart
+++ lib/add_record.dart
@@ -1,4 +1,4 @@
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class _AddRecordsPageState extends State<AddRecordsPage> {
@@ -7,49 +7,114 @@ class _AddRecordsPageState extends State<AddRecordsPage> {
   int _benchRM = 0;
   int _deadliftRM = 0;
   int _pressRM = 0;
+  String _date = "20180908";
 
   @override
   Widget build(BuildContext ctxt) {
+    void addData() async {
+      await Firestore.instance.collection('MaxReps').add({
+        'squat': _squatRM,
+        'bench': _benchRM,
+        'deadlift': _deadliftRM,
+        'press': _pressRM,
+        'date': _date,
+      });
+    }
+
     return Scaffold(
       appBar: new AppBar(title: new Text("Add new 1 RM records!")),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TextFormField(
-              keyboardType: TextInputType.number,
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                final n = num.tryParse(value);
-                if (n == null) {
-                  return 'Numbers should be greater than 0';
-                }
-              },
-              decoration: InputDecoration(
-                labelText: 'Enter 1RM for squat',
-                prefixIcon: Icon(Icons.assignment),
-              ),
-              onSaved: (input) => _squatRM = int.parse(input),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: RaisedButton(
-                onPressed: () {
-                  // Validate will return true if the form is valid, or false if
-                  // the form is invalid.
-                  if (_formKey.currentState.validate()) {
-                    // If the form is valid, we want to show a Snackbar
-                    Scaffold.of(ctxt).showSnackBar(
-                        SnackBar(content: Text('Processing Data')));
+      body: Padding(
+        padding: EdgeInsets.only(top: 20, right: 20, left: 20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  final n = num.tryParse(value);
+                  if (n == null) {
+                    return 'Please enter a number';
                   }
                 },
-                child: Text('Submit'),
+                decoration: InputDecoration(
+                  labelText: 'Enter 1RM for squat',
+                  prefixIcon: Icon(Icons.assignment),
+                ),
+                onSaved: (input) => _squatRM = int.parse(input),
               ),
-            ),
-          ],
+              TextFormField(
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  final n = num.tryParse(value);
+                  if (n == null) {
+                    return 'Please enter a number';
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Enter 1RM for bench',
+                  prefixIcon: Icon(Icons.assignment),
+                ),
+                onSaved: (input) => _benchRM = int.parse(input),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  final n = num.tryParse(value);
+                  if (n == null) {
+                    return 'Please enter a number';
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Enter 1RM for deadlift',
+                  prefixIcon: Icon(Icons.assignment),
+                ),
+                onSaved: (input) => _deadliftRM = int.parse(input),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  final n = num.tryParse(value);
+                  if (n == null) {
+                    return 'Please enter a number';
+                  }
+                },
+                decoration: InputDecoration(
+                  labelText: 'Enter 1RM for press',
+                  prefixIcon: Icon(Icons.assignment),
+                ),
+                onSaved: (input) => _pressRM = int.parse(input),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // Validate will return true if the form is valid, or false if
+                    // the form is invalid.
+                    if (_formKey.currentState.validate()) {
+                      // If the form is valid, submit data to database
+                      _formKey.currentState.save();
+                      addData();
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
