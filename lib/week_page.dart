b6973da82b83d9b1242ded8be6e9998f4ff3b738
Justin K -Mac <justinjoseph0007@gmail.com>
2019-05-23 18:30:40 -0400

    Login and Logout Stages works now

--- lib/week_page.dart
+++ lib/week_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'auth.dart'; // To sign in with Google
 import 'side_drawer.dart';
 import 'week_widget.dart';
 
-class WeekPage extends StatelessWidget {
+class WeekPage extends StatefulWidget {
+  WeekPage({
+    Key key,
+  }) : super(key: key);
+
+  final _WeekPageWidgetState state = new _WeekPageWidgetState();
+
+  @override
+  _WeekPageWidgetState createState() => state;
+}
+
+class _WeekPageWidgetState extends State<WeekPage> {
+  String userPhotoUrl = "null";
+
   Future<bool> _loginUser() async {
     final api = await FBApi.signInWithGoogle();
     if (api != null) {
@@ -11,6 +25,32 @@ class WeekPage extends StatelessWidget {
     } else {
       return false;
     }
+  }
+
+  Future<void> _logoutUser() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  Future<bool> _checkIfUserIsLoggedIn() async {
+    final FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    if (user != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void _getUserPhotoUrl() async {
+    final FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    setState(() {
+      userPhotoUrl = user.photoUrl.toString();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserPhotoUrl();
   }
 
   Weeks week1 = new Weeks(
@@ -42,25 +82,97 @@ class WeekPage extends StatelessWidget {
     week4.update();
   }
 
-  // TODO: Add a circle avatar button that diplays user image when signed in and show dialog box to sign out if clicked after signing in
   @override
   Widget build(BuildContext ctxt) {
+    void _showLogoutDialog() {
+      // flutter defined function
+      showDialog(
+        context: ctxt,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: Text("Do you want to logout?"),
+            actions: <Widget>[
+              // Usually buttons at the bottom of the dialog
+              FlatButton(
+                child: Text(
+                  "Yes",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {
+                  _logoutUser();
+                  // Exit out of the window after reseting
+                  setState(() {}); // Refresh page
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Text(
+                  "Cancel",
+                  style: TextStyle(
+                    color: Theme.of(context).hintColor,
+                  ),
+                ),
+                onPressed: () {
+                  // Exit out of the window to cancel
+                  Navigator.of(context).pop();
+                },
+              ),
+              SizedBox(width: 10), // Add a little bit of padding after
+            ], // Actions ends here
+          );
+        },
+      );
+    }
+
     return Scaffold(
       drawer: SideDrawer(callBackWeeksPage: this.update),
       appBar: AppBar(
         title: Text("Select week in your routine"),
         actions: <Widget>[
           new Builder(builder: (BuildContext ctxt) {
-            return IconButton(
+            Widget _loginButton = IconButton(
               icon: Icon(Icons.account_circle),
               onPressed: () async {
                 const _pass_msg = SnackBar(content: Text('Signed with google'));
                 const _fail_msg = SnackBar(content: Text('Signing in failed!'));
                 bool _isLoggedIN = await _loginUser();
+                setState(() {}); // Refresh page
                 _isLoggedIN
                     ? Scaffold.of(ctxt).showSnackBar(_pass_msg)
                     : Scaffold.of(ctxt).showSnackBar(_fail_msg);
               },
+            );
+
+            Widget _logoutButton = GestureDetector(
+              onTap: _showLogoutDialog,
+              child: Padding(
+                padding: EdgeInsets.only(right: 15),
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 15,
+                    backgroundImage: NetworkImage(userPhotoUrl),
+                  ),
+                ),
+              ),
+            );
+
+            return FutureBuilder<bool>(
+              future: _checkIfUserIsLoggedIn(),
+              initialData: false,
+              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                if (snapshot.hasError) {
+                  return _loginButton;
+                } else {
+                  if (snapshot.data == true) {
+                    return _logoutButton;
+                  } else {
+                    return _loginButton;
+                  }
+                }
+              }, // End of  builder
             );
           }),
         ],
