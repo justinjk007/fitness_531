60fef665a2febd95f9fe0f632dd1dd0d516fc8cf
Justin K -W10 <justinjoseph0007@gmail.com>
2019-05-26 23:11:30 -0400

    I will activate links later

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+// import 'package:url_launcher/url_launcher.dart';
 
+// TODO: activate links
 class AboutPage extends StatelessWidget {
+  // _launchURL(String url) async {
+  //   if (await canLaunch(url)) {
+  //     await launch(url);
+  //   }
+  // }
+
   @override
   Widget build(BuildContext ctxt) {
     return new Scaffold(
@@ -16,6 +25,22 @@ class AboutPage extends StatelessWidget {
                 "stop crunching percentages and calculating plate counts at the gym for the 5/3/1 fitness regimen. "
                 "Now daily workout routines are already calculated and tracked, I can just"
                 " focus on my training.",
+              ),
+              SizedBox(height: 30),
+              FlatButton.icon(
+                // onPressed: _launchURL("mailto:justinjoseph0007@gmail.com"),
+                icon: Icon(EvaIcons.emailOutline),
+                label: Text("Email me"),
+              ),
+              FlatButton.icon(
+                // onPressed: _launchURL("https://twitter.com/justinjk007"),
+                icon: Icon(EvaIcons.twitterOutline),
+                label: Text("Twitter"),
+              ),
+              FlatButton.icon(
+                // onPressed: _launchURL("https://github.com/justinjk007"),
+                icon: Icon(EvaIcons.githubOutline),
+                label: Text("Github"),
               ),
             ],
           ),
